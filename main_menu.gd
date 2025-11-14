1802e8ca35a73cb72e278ece95b13f3914df5a0c
GinoFrappicino <Gino.Conca@colostate.edu>
2025-11-14 01:47:37 -0700

    Added Main Menu, Funny Music, SFX, Volume Slider, and Rendering

---
+++ main_menu.gd
@@ -0,0 +1,24 @@
+extends Control
+@onready var main_buttons: VBoxContainer = $MainButtons
+@onready var options: Panel = $Options
+
+func _process(_delta):
+	pass
+	
+func _ready():
+	main_buttons.visible = true
+	options.visible = false
+	
+func _on_start_pressed():
+	get_tree().change_scene_to_file("res://main.tscn")
+	
+func _on_settings_pressed():
+	print("Settings pressed")
+	main_buttons.visible = false
+	options.visible = true
+	
+func _on_exit_pressed():
+	get_tree().quit()
+
+func _on_back_options_pressed() -> void:
+	_ready()
