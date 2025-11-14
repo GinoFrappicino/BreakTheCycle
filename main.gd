5ff7fca3d3f4bff4dba89eba03907d1f525527bc
Redpando2005 <bcartwright2005@gmail.com>
2025-11-14 12:47:52 -0700

    More fix on inventory item interaction

--- main.gd
+++ main.gd
@@ -1,18 +1,23 @@
 extends Node2D
 
-var item = preload("res://Items/item.tscn")
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	spawn_initial_items()
+	remove_picked_up_items_from_scene()
 
-func spawn_initial_items():
-	var item = item.instantiate()
-	Inventory.add_item(item)  # this makes it persist
-
-
-
+func remove_picked_up_items_from_scene():
+	# Loop through all children in the scene
+	for child in get_children():
+		# Check if child is an item (assuming all items extend Sprite2D and have item_id)
+		if child is Sprite2D and child.has_method("item_id"):
+			# Check if this item is already in Inventory
+			var already_in_inventory = false
+			for slot_item in Inventory.slots:
+				if slot_item != null and slot_item == child:
+					already_in_inventory = true
+					break
+			# If item is in inventory, remove it from the scene
+			if already_in_inventory:
+				remove_child(child)
+				
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://houseRight.tscn")
 
